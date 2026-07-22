038241651d990b6eefd86cf9f55ed2cd3751761c
Fadhlin Qistina <fadhlinqistina@usm.my>
2026-07-22 22:02:12 +0800

    Fix 500 error by disabling forceGlobalSecureRequests

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     //public string $baseURL = 'http://localhost:8080/';
-    public string $baseURL = '[https://mealvoucher.site/](https://mealvoucher.site/)';
+    public string $baseURL = 'https://mealvoucher.site/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -161,7 +161,7 @@ class App extends BaseConfig
      * and the HTTP Strict Transport Security (HSTS) header will be set.
      */
     //public bool $forceGlobalSecureRequests = false;
-    public bool $forceGlobalSecureRequests = true;
+    public bool $forceGlobalSecureRequests = false;
 
     /**
      * --------------------------------------------------------------------------
